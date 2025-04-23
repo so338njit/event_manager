26f9e844fc7116ff0a290cba21104da0983e49de
Sean O'Donnell <so338@njit.edu>
2025-04-23 19:42:15 -0400

    corrected syntax in user update data fixture to match test

--- tests/conftest.py
+++ tests/conftest.py
@@ -259,6 +259,8 @@ def user_create_data(user_base_data):
 def user_update_data():
     return {
         "email": "john.doe.new@example.com",
+        "first_name": "John",
+        "last_name": "Doe",
         "full_name": "John H. Doe",
         "bio": "I specialize in backend development with Python and Node.js.",
         "profile_picture_url": "https://example.com/profile_pictures/john_doe_updated.jpg"
